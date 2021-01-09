8b7e7f6fc413975893f6767a5690f6003548f25e
Yunus Emre Deligöz <ye@deligoz.me>
2021-01-09 20:27:52 +0300

    Create TaskTest.php

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Task;
+use Laravel\Passport\Passport;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+}
