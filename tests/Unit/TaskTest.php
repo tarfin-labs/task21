49b446069d8345cd80461f362c234114d33a5d91
Yunus Emre Deligöz <ye@deligoz.me>
2021-01-09 20:26:54 +0300

    Create TaskTest.php

---
+++ tests/Unit/TaskTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+}
