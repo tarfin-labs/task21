b7fc7e83ea6f1db46d8c2cba80b1a455f6b26504
Yunus Emre Deligöz <ye@deligoz.me>
2021-01-09 20:28:17 +0300

    TaskTest: Add `an_authenticated_user_can_create_a_task()` test

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -12,4 +12,31 @@ class TaskTest extends TestCase
 {
     use RefreshDatabase;
 
+    /** @test */
+    public function an_authenticated_user_can_create_a_task(): void
+    {
+        $user = User::factory()->create();
+
+        Passport::actingAs($user);
+
+        $attributes = Task::factory()->raw();
+
+        $response = $this->postJson('api/tasks', $attributes);
+
+        $response
+            ->assertCreated()
+            ->assertJsonStructure([
+                                      'data' => [
+                                          'title',
+                                          'description',
+                                          'status',
+                                          'user_id',
+                                      ],
+                                  ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'title'       => $attributes['title'],
+            'description' => $attributes['description'],
+        ]);
+    }
 }
